944a1baed39d15952717590558aa78f46b83458f
gawde-abhijeet <gawde_abhijeet@hotmail.com>
2016-03-16 15:39:30 +0530

    Added Claudia JS Deployment Project

---
+++ deploy-CloudiaJS/lambda-users/api-lambda.js
@@ -0,0 +1,27 @@
+﻿/* global require, module */
+
+var ApiBuilder = require("claudia-api-builder");
+
+var api = new ApiBuilder(),
+    Promise = require('bluebird'),
+    AWS = require('aws-sdk'),
+    DOC = require("dynamodb-doc"),
+    dynamo = require('./dynamo-users');
+
+// get all users
+api.get('/users', function (request) {
+    'use strict';
+    
+    return dynamo.listAllUsers(request.env.tableName);
+
+});
+
+// get user for {id}
+api.get('/users/{id}', function (request) {
+    'use strict';
+    
+    return dynamo.getUserById(request.pathParams.id, request.env.tableName);
+
+});
+
+module.exports = api;
---
+++ deploy-CloudiaJS/lambda-users/dynamo-users.js
@@ -0,0 +1,37 @@
+﻿
+var Promise = require('bluebird'),
+    AWS = require('aws-sdk'),
+    DOC = require("dynamodb-doc");
+
+var docClient = Promise.promisifyAll(new DOC.DynamoDB())
+
+var dynamoListAllUsers = function (reqTableName) {
+    
+    // Set up event for dynamo
+    var params = {
+        TableName: getTableName(reqTableName)
+    };
+
+    return docClient.scanAsync(params);
+} 
+
+var dynamoUserById = function (reqUserId, reqTableName) {
+    
+    // Set up event for dynamo
+    var params = {
+        TableName: getTableName(reqTableName),
+        Key: {
+            userId: reqUserId
+        }
+    };
+    
+    return docClient.getItemAsync(params);
+}
+
+
+function getTableName(tableName) {
+    return tableName || "lambda-users";
+};
+
+module.exports.listAllUsers = dynamoListAllUsers;
+module.exports.getUserById = dynamoUserById;
