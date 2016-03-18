3d30e6d4dcbc6730865b8288505ea0cba12366af
gawde-abhijeet <gawde_abhijeet@hotmail.com>
2016-03-18 18:49:26 +0530

    Incorporating the Mocha for unit testing strategy. Initial setup done and test basic working. TODO: Need to implement the actual unit testing code.

---
+++ lambdaDynamoDB/test/comments-unit-test.js
@@ -0,0 +1,9 @@
+﻿var assert = require('assert');
+
+describe("A Feature", function () {
+    describe("A Scenario", function () {
+        it("does something expected", function () { 
+            assert(true); 
+        }); 
+    }); 
+});
