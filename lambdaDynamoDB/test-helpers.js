f9d780442934201cdce98ab07609e12036ccf4c5
gawde-abhijeet <gawde_abhijeet@hotmail.com>
2016-03-24 21:47:17 +0530

    including test-helpers.js

---
+++ lambdaDynamoDB/test-helpers.js
@@ -0,0 +1,21 @@
+﻿var context = require('aws-lambda-mock-context')();
+
+exports.invokeLambdaFunc = function invokeLambdaFunc(lambdafunc, eventjson) {
+    
+    var fs = require('fs');
+    var lambda = require(lambdafunc);
+    var resp1 = null;
+    
+
+    // Load the event to be passed to Lambda what you want Lambda to process on.
+    var event = JSON.parse(fs.readFileSync(eventjson, 'utf8').trim());
+    
+    var result =  lambda.handler(event, context);
+
+    // Captures the response and/or errors
+    context.Promise
+        .then(function (value) { done(null, value); })
+        .catch(function (err) { done(err); });
+
+    return result;
+};
